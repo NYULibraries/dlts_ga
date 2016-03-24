a398209cdbabc05ad197392dbc6808a0467057b9
Alberto Ortiz Flores <aortiz@nyu.edu>
2016-03-24 17:40:00 -0400

    Only send the first collection for dimension1. send page view if debug mode is off

--- js/dlts_ga.js
+++ js/dlts_ga.js
@@ -7,16 +7,26 @@
         if (enable === 1 && jQuery.isFunction(window.ga)) {
           window.ga("create", settings.dlts_ga.ua, { "cookieDomain": settings.dlts_ga.cookieDomain });
           window.ga("set", "anonymizeIp", settings.dlts_ga.anonymizeIp);
-          if (jQuery.isArray(settings.dlts_ga.collections)) {
+          // https://jira.nyu.edu/browse/DLTSVIDEO-83 
+          // https://jira.nyu.edu/browse/DLTSVIDEO-67
+          /**
+          if (jQuery.isArray(settings.dlts_ga.dimensions.collections)) {
             $.each(settings.dlts_ga.collections, function(key, collection) {
               window.ga("set", "dimension1", collection.name);
             });            
           }
+          */
+          /* we only send the first item in the collections dimensions */
+          if (jQuery.isArray(settings.dlts_ga.dimensions.collections)) {
+            window.ga("set", "dimension1", settings.dlts_ga.dimensions.collections[0].name);
+          }          
           if (debug) {
-            console.log(GoogleAnalyticsObject);
+            console.log('GoogleAnalyticsObject: ' + GoogleAnalyticsObject);
             console.log(settings.dlts_ga);
           }
-          window.ga("send", "pageview");
+          else {
+            window.ga("send", "pageview");
+          }
         }
       }
     }    
